94c66f099fbf928ed15a6334a62eab24d4c302be
yoshimin <drop.under.the.rose@gmail.com>
2013-08-24 01:19:28 +0900

    Initial Commit

---
+++ BounceMenu/YSAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  YSAppDelegate.h
+//  BounceMenu
+//
+//  Created by Shingai Yoshimi on 8/24/13.
+//  Copyright (c) 2013 Shingai Yoshimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class YSViewController;
+
+@interface YSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) YSViewController *viewController;
+
+@end
